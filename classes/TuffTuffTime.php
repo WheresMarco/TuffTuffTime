3e7e0b9386788b854a3f044be0d55f70c6c44ef3
Marco Hyyryläinen <marco@wheresmar.co>
2014-10-14 14:03:27 +0200

    Accidentally show arriving in departing and vice versa

--- classes/TuffTuffTime.php
+++ classes/TuffTuffTime.php
@@ -35,48 +35,6 @@
               "<QUERY objecttype='TrainAnnouncement' orderby='AdvertisedTimeAtLocation'>" .
                 "<FILTER>" .
                   "<AND>" .
-                  "<EQ name='ActivityType' value='Avgang' />" .
-                  "<EQ name='LocationSignature' value='" . $this->stationID . "' />" .
-                  "<OR>" .
-                      "<AND>" .
-                        "<GT name='AdvertisedTimeAtLocation' value='\$dateadd(-00:15:00)' />" .
-                        "<LT name='AdvertisedTimeAtLocation' value='\$dateadd(14:00:00)' />" .
-                      "</AND>" .
-                      "<AND>" .
-                        "<LT name='AdvertisedTimeAtLocation' value='\$dateadd(00:30:00)' />" .
-                        "<GT name='EstimatedTimeAtLocation' value='\$dateadd(-00:15:00)' />" .
-                      "</AND>" .
-                    "</OR>" .
-                  "</AND>" .
-                "</FILTER>" .
-              "</QUERY>" .
-              "</REQUEST>";
-
-      // Open up curl session and fire of the request
-      $session = curl_init();
-      curl_setopt_array($session, self::$options);
-      curl_setopt($session, CURLOPT_POSTFIELDS, "$xml");
-      $response = curl_exec($session);
-      curl_close($session);
-
-      // Check if we got a response
-      if(!$response)
-        throw new \Exception("Could not get stations");
-
-      return $response;
-    }
-
-    /**
-      * Get departing trains/busses(?) to the station
-      *
-      * @return array
-      */
-    public function getDeparting() {
-      $xml = "<REQUEST>" .
-              "<LOGIN authenticationkey='". \Settings::$apiKey ."' />" .
-              "<QUERY objecttype='TrainAnnouncement' orderby='AdvertisedTimeAtLocation'>" .
-                "<FILTER>" .
-                  "<AND>" .
                   "<EQ name='ActivityType' value='Ankomst' />" .
                   "<EQ name='LocationSignature' value='" . $this->stationID . "' />" .
                   "<OR>" .
@@ -105,7 +63,49 @@
       if(!$response)
         throw new \Exception("Could not get stations");
 
-      return $response;
+      return json_decode($response, true);
+    }
+
+    /**
+      * Get departing trains/busses(?) to the station
+      *
+      * @return array
+      */
+    public function getDeparting() {
+      $xml = "<REQUEST>" .
+              "<LOGIN authenticationkey='". \Settings::$apiKey ."' />" .
+              "<QUERY objecttype='TrainAnnouncement' orderby='AdvertisedTimeAtLocation'>" .
+                "<FILTER>" .
+                  "<AND>" .
+                  "<EQ name='ActivityType' value='Avgang' />" .
+                  "<EQ name='LocationSignature' value='" . $this->stationID . "' />" .
+                  "<OR>" .
+                      "<AND>" .
+                        "<GT name='AdvertisedTimeAtLocation' value='\$dateadd(-00:15:00)' />" .
+                        "<LT name='AdvertisedTimeAtLocation' value='\$dateadd(14:00:00)' />" .
+                      "</AND>" .
+                      "<AND>" .
+                        "<LT name='AdvertisedTimeAtLocation' value='\$dateadd(00:30:00)' />" .
+                        "<GT name='EstimatedTimeAtLocation' value='\$dateadd(-00:15:00)' />" .
+                      "</AND>" .
+                    "</OR>" .
+                  "</AND>" .
+                "</FILTER>" .
+              "</QUERY>" .
+              "</REQUEST>";
+
+      // Open up curl session and fire of the request
+      $session = curl_init();
+      curl_setopt_array($session, self::$options);
+      curl_setopt($session, CURLOPT_POSTFIELDS, "$xml");
+      $response = curl_exec($session);
+      curl_close($session);
+
+      // Check if we got a response
+      if(!$response)
+        throw new \Exception("Could not get stations");
+
+      return json_decode($response, true);
     }
 
     /**
@@ -157,6 +157,29 @@
       // No match? Throw a exception
       if ($foundID === "")
         throw new \Exception("Could not find ID in returned array. Must be name exact name (ex. Stockholm C)");
+
+      return $foundID;
+    }
+
+    /**
+      * Get the name of a station from id.
+      *
+      * @param string $id - ID of the station
+      * @return string
+      */
+    public function getStationName($id) {
+      $foundID = "";
+
+      // // Loop through the returned array to find the name
+      foreach($this->stations['RESPONSE']['RESULT']['0']['TrainStation'] as $station) {
+        if (array_search($id, $station)) {
+          $foundID = $station['AdvertisedLocationName'];
+        }
+      }
+
+      // No match? Throw a exception
+      if ($foundID === "")
+        throw new \Exception("Could not find the name in returned array.");
 
       return $foundID;
     }
