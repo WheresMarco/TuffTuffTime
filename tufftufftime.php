b8f312353ffdcc6adde2f8fedb6a0cee8b257759
Marco Hyyryläinen <marco@wheresmar.co>
2016-10-13 17:27:43 +0200

    Right version number

--- tufftufftime.php
+++ tufftufftime.php
@@ -16,7 +16,7 @@
  * Plugin Name:       TuffTuffTime
  * Plugin URI:        https://github.com/WheresMarco/TuffTuffTime
  * Description:       A WordPress widget that shows the current and future stops at a train station in Sweden.
- * Version:           1.0.0
+ * Version:           2.0.0
  * Author:            Marco Hyyryläinen
  * Author URI:        http://wheresmar.co
  * License:           GPL-2.0+
